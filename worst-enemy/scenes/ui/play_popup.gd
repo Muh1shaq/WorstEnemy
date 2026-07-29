8521887895df951efb28f46673536b2af3188694
Muh1shaq <user426981@gmail.com>
2026-07-29 13:34:19 +0800

    game update

---
+++ worst-enemy/scenes/ui/play_popup.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal new_game_pressed
+signal continue_pressed
+
+@onready var new_game_button: Button = $Frame/VBoxContainer/NewGameButton
+@onready var continue_button: Button = $Frame/VBoxContainer/ContinueButton
+
+func _ready() -> void:
+	hide() # Sembunyikan pop-up saat awal game berjalan
+	
+	# Hubungkan tombol internal ke signal
+	if new_game_button:
+		new_game_button.pressed.connect(func(): new_game_pressed.emit())
+	if continue_button:
+		continue_button.pressed.connect(func(): continue_pressed.emit())
+
+# Fungsi untuk membuka pop-up (dipanggil dari main_screen)
+func open_popup() -> void:
+	show()
+
+# Menutup pop-up saat mengklik area luar (Background)
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		hide()
--- worst-enemy/scripts/main_screen.gd
+++ worst-enemy/scripts/main_screen.gd
@@ -1,9 +1,14 @@
 extends Control
-
+@onready var play_button: TextureButton = $VBoxContainer/PlayButton
+@onready var play_popup: Control = $PlayPopUp
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_button_pressed)
+	play_popup.new_game_pressed.connect(_on_start_new_game)
+	play_popup.continue_pressed.connect(_on_continue_game)
 func _on_play_button_pressed() -> void:
-	# Mengubah scene ke Loading Screen
-	get_tree().change_scene_to_file("res://scenes/ui/loading_screen.tscn")
-
-func _on_exit_button_pressed() -> void:
-	# Keluar dari aplikasi
-	get_tree().quit()
+	play_popup.open_popup()
+func _on_start_new_game() -> void:
+	print("Memulai Game Baru...")
+	get_tree().change_scene_to_file("res://scenes/loading_screen.tscn") # Sesuaikan path scene Anda
+func _on_continue_game() -> void:
+	print("Melanjutkan Game...")
